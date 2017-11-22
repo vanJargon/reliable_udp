40ba304765cc4175cd996db5a269f847c136b843
weixuan_lim <enigmawx@gmail.com>
2017-11-22 21:26:23 +0800

    Sender Address Edit

--- TCPSender.py
+++ TCPSender.py
@@ -18,7 +18,7 @@ def main(rate, address, filename):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
-    server_address = ('localhost', 10000)
+    server_address = (address, 10000)
     print('connecting to {} port {}'.format(*server_address))
     sock.connect(server_address)
 
