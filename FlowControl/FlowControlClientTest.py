d20a057b65dca179c3fd81438f4f435002c51aa8
Vanessa Tan <vanessa_yun@mymail.sutd.edu.sg>
2017-11-23 11:10:26 +0800

    Add files via upload

--- FlowControl/FlowControlClientTest.py
+++ FlowControl/FlowControlClientTest.py
@@ -1,20 +1,229 @@
-import datetime, time
+#!/usr/bin/python3
+
+"""
+Client code.
+
+Packet layout
+|IP header (20bytes)|UDP header (8bytes)|Payload|
+
+Payload layout
+|f_nack(4bits)|f_fin(1bit)|f_ack(1bit)|f_endTransaction(1bit)|f_newTransaction(1bit)|transactionId(4bytes)|segId(4bytes)|data|
+
+Assuming server buffer is fixed at 65535 bytes. 
+Max data size = 65535 - 20 - 8 - 1 - 4 - 4 = 65498
+
+1.0Mbps = 125kBps
+Sending packets of size BB Bytes, we have to send a packet every (BB Bytes)/(1.0*125000Bps) seconds
+Or, time interval between packets = (BB Bytes)/(rate*125000Bps)
+
+Use -r to select rate
+Use -a to select address to send data to, default is localhost
+Use -f to select file to send, default is a short message
+
+On mininet, to send data to another host, please use -a <other host IP>, e.g. python ./client.py -a 10.0.0.2
+"""
+
 from socket import *
+import argparse
+from os import urandom
+from struct import *
+import time
+import sys
 
-for pings in range(10):
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    clientSocket.settimeout(1)
-    message = "test"
-    addr = ("127.0.0.1", 12000)
+def main(rate, address, filename):
+    global safeConnect
+    global dataLoss
+    global factor
+    global mtu
+    if filename:
+        with open(filename, 'rb') as input_file:
+            message = input_file.read()
+    else:
+        message = 'hi how are you'.encode('UTF-8')
+    
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.settimeout(0.1)
+    receiver_address = (address, 5555)
+    
+    def segsend(segId, data_payload_size):
+        
+        msg = message[segId:segId+data_payload_size] # spliced file contents
+        msg_length = len(msg)
+        f_newTransaction = 1 if segId == 0 else 0
+        f_endTransaction = 1 if segId+data_payload_size >= len(message) else 0
+        f_ack = 0
+        f_fin = 0 if window else 1
+        flags = f_newTransaction + (f_endTransaction << 1) + (f_ack << 2) + (f_fin << 3)
+        #print(flags)
+        #print('msg length %d' % msg_length)
+        payload = pack('!BIII' + str(msg_length) + 's', flags, tr_id, segId, len(message), msg) # struct pack is used to make sure that segId is always 4 bytes large, since integers in python are always 4 bytes
+        sent = sock.sendto(payload, receiver_address)
+        
+        #print('segId:',segId,' sent:',sent-9)
+    
+    starttime = time.time()
+    
+    mtu = 6400
+    factor = 1
+    safeConnect = 5
+    dataLoss = 0
+    packet_size = mtu * factor
+    if packet_size > 65535:
+        packet_size = 65535
+    else:
+        packet_size = packet_size
 
-    start = datetime.datetime.now()
-    clientSocket.sendto(message.encode(), addr)
-    try:
-        data, server = clientSocket.recvfrom(1024)
-        end = datetime.datetime.now()
-        elapsed = end - start
-        print('%s %d %d' % (data, pings, elapsed.microseconds))
-        time.sleep(1)
-    except timeout:
-        print("REQUEST TIMED OUT")
-        time.sleep(1)
+    data_payload_size = packet_size - 28 - 13
+    
+    timeinterval = packet_size/(rate*125000.0)
+    
+    nexttime = starttime - timeinterval # to make the client start sending data immediately instead of waiting for timeinterval
+    
+    segId = 0
+    tr_id = 353 # should be random
+    max_window_size = 5
+    
+    window = [] # composite window. Only segIds that have not been ACKed will be in the window
+    global nextSegId
+    nextSegId = 0 # the next segment id to add into the window
+    window_timer = []
+    w_timeout = 0.2
+    
+    # add stuff into the window
+    def addToWindow(nextSegId):
+        while len(window) < max_window_size and nextSegId < len(message):
+
+            global dataLoss
+            dataLoss -= 1
+            
+
+            if nextSegId + data_payload_size >= len(message):
+                data_size = len(message)-nextSegId
+            else:
+                data_size = data_payload_size
+            window.append((nextSegId, data_size))
+            nextSegId += data_size
+            window_timer.append(time.time() - w_timeout)
+        return nextSegId
+    
+    nextSegId = addToWindow(nextSegId)
+    
+    def listenForAcks():
+        global safeConnect
+        global dataLoss
+        global factor
+        global packet_size
+        global data_payload_size
+        global mtu
+        if safeConnect > 5:
+            safeConnect = 0
+            factor += 1
+            
+            packet_size = mtu * factor
+            if packet_size > 65535:
+                packet_size = 65535
+            else:
+                packet_size = packet_size
+                data_payload_size = packet_size - 28 - 13
+            
+            
+        
+        try:
+            data, addr = sock.recvfrom(1024)
+             
+            safeConnect += 1
+            flags, recSegId, recMsgLen = unpack('!BII', data)
+            f_fin = flags & 2**3
+            #print('fin',f_fin)
+            #print('window',window)
+            #print('segId',recSegId)
+            #print('msglen',recMsgLen)
+            if recSegId < len(message):
+                indexToChange = window.index((recSegId, recMsgLen))
+                window.pop(indexToChange)
+                window_timer.pop(indexToChange)
+                global nextSegId
+                nextSegId = addToWindow(nextSegId)
+            #print("\nreceived message:", data,"\nflags:", flags, "\nrecSegId:", recSegId, "\nrecMsgLen:", recMsgLen)
+            #print('w2',window)
+            if f_fin:
+                return True
+        except timeout:
+            pass
+        return False
+
+    while window:
+        if listenForAcks():
+            break
+        if (time.time() - nexttime) < timeinterval: continue
+        nexttime += timeinterval
+        
+        for i in range(len(window)):
+            if time.time() - window_timer[i] > w_timeout:
+                
+                dataLoss += 1
+                
+                if dataLoss > 3:
+                    safeConnect = 0
+                    if factor <= 1:
+                        factor = 1
+                    else:   
+                        factor -= 1
+                    dataLoss = 0
+                    packet_size = mtu * factor
+                    if packet_size > 65535:
+                        packet_size = 65535
+                    else:
+                        packet_size = packet_size
+
+                        data_payload_size = packet_size - 28 - 13
+                print(factor)
+                print(safeConnect)
+                print(dataLoss)
+
+
+                segId, data_payload_size = window[i]
+                segsend(segId, data_payload_size)
+                window_timer[i] = time.time()
+
+        #print('flags:',flags,'\npayload:',payload)
+        #print('sent %d bytes' % (sent))
+        sys.stdout.write("\rTime elapsed: %.3fs" % (time.time()-starttime))
+        sys.stdout.flush()
+    
+    window_timer.append(time.time() - w_timeout)
+    while True:
+        if listenForAcks():
+            break
+        if (time.time() - nexttime) < timeinterval: continue
+        nexttime += timeinterval
+        
+        if time.time() - window_timer[0] > w_timeout:
+            segsend(len(message), 0)
+            window_timer[0] = time.time()
+
+        #print('flags:',flags,'\npayload:',payload)
+        #print('sent %d bytes' % (sent))
+        sys.stdout.write("\rTime elapsed: %.3fs" % (time.time()-starttime))
+        sys.stdout.flush()
+    
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', type=float, dest='rate', default=3.0, 
+        help='Packet rate in Mbps (eg; -r 1.5 is 1.5 Mbps)')
+    
+    parser.add_argument('-a', type=str, dest='address', default='localhost', 
+        help='Indicate the ip address to send the data to')
+    
+    parser.add_argument('-f', type=str, dest='filename', default='', 
+        help='Indicate the name of file to send')
+    
+    args = parser.parse_args()
+    
+    print("Data rate is %.1f Mbps\nSending to %s" % (args.rate, args.address))
+    main(args.rate, args.address, args.filename)
+
+
+
--- FlowControl/FlowControlServerTest.py
+++ FlowControl/FlowControlServerTest.py
@@ -1,15 +1,93 @@
-import random
-from socket import *
+#!/usr/bin/python3
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', 12000))
+"""
+Server code.
 
-while True:
-    rand = random.randint(0, 10)
-    message, address = serverSocket.recvfrom(1024)
-    message = message.upper()
-    if rand >= 4:
-        serverSocket.sendto(message, address) 
-        print("rand >= 4")
-    else:
-    	print("rand < 4")
+Socket binds to 0.0.0.0 instead of localhost so it responds to its public IP address
+
+As our protocol runs on top of UDP, we have the following structure
+Header layout
+|f_nack(4bits)|f_fin(1bit)|f_ack(1bit)|f_endTransaction(1bit)|f_newTransaction(1bit)|transactionId(4bytes)|segId(4bytes)|data|
+
+New filestreams must start with segId=0, with f_newTransaction=1
+Subsequent segIds simply add the current segId and the number of msg bytes received
+
+The server either acks or nacks every 10 messages received from the same IP,Port,TransactionId
+
+Additional cmd line option --verbose added to make server noisy
+"""
+
+import socket
+from struct import *
+import argparse
+
+def run_server(verbose, savefile, output_filename):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#socket.SOCK_RAW, socket.IPPROTO_IP)#
+    server_address = '0.0.0.0'
+    server_port = 5555
+    sock.bind((server_address, server_port))
+    
+    
+    if verbose:
+        print("Server started. Listening on %s at port %d" % (server_address, server_port))
+    talkedTo = {}
+    s = bytearray()
+    
+    while True:
+        recv = sock.recvfrom(65535)
+        data, client_addr = recv
+        client_ip = client_addr[0]
+        msg_length = len(data) - 13
+        flags, tr_id, segId, total_len, msg = unpack('!BIII' + str(msg_length) + 's', data)
+        
+        fin = flags & 2**3
+        f_newTransaction = flags & 2**0
+        if f_newTransaction:
+            print('new filestream started')
+            s = bytearray(total_len)
+        
+        # store data into s TODO: make it such that data stored is unique to transaction_id, source_ip and source_port
+        if msg_length:
+            s[segId:segId+msg_length] = msg
+        
+        
+        #print('received %d bytes of UDP payload from %s' % (len(data), str(client_addr)))
+        
+        if verbose:
+            #print("Data: {}".format(data))
+            #print("Addresses: {}".format(client_addr))
+            print("segId:%d,msg_length:%d" % (segId, msg_length))
+            print('flags:', flags)
+            print('f_newTransaction:', flags & 2**0)
+            print('f_endTransaction:', (flags & 2**1) >> 1)
+            print('f_fin:', (flags & 2**3) >> 3)
+        
+        talkedTo[client_ip] = int(segId) + int(msg_length)
+        
+        f_endTransaction = (flags & 2**1) >> 1
+        f_newTransaction = 0
+        f_ack = 0
+        f_fin = 1 if fin else 0
+        flags = f_newTransaction + (f_endTransaction << 1) + (f_ack << 2) + (f_fin << 3)
+
+        payload = pack('!BII', flags, segId, msg_length)
+        sock.sendto(payload, client_addr)
+        #print('sent',payload,'to',client_addr)
+        #print('received %d bytes of actual data in total' % (len(s)))
+        
+        if fin:
+            print('endTr, file size:', len(s))
+            if savefile:
+                with open(output_filename, 'wb') as of:
+                    of.write(s)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Prints data and addresses')
+    parser.add_argument('-s', '--savefile', action='store_true', default=False, help='Indicate whether to save data to file')
+    parser.add_argument('-o', dest='output_filename', default='output', help="Indicate name of file to output. Defaults to 'output' in the same directory")
+    
+    args = parser.parse_args()
+
+    run_server(args.verbose, args.savefile, args.output_filename)
+        
