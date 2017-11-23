5f601a186cbae79077d38b0cc46a2c756b0217fa
privatejw <privatejw@gmail.com>
2017-11-23 19:53:54 +0800

    updated sender to look cool in code

--- sender.py
+++ sender.py
@@ -18,151 +18,156 @@ Or, time interval between packets = (BB Bytes)/(rate*125000Bps)
 
 Use -r to select rate
 Use -a to select address to send data to, default is localhost
-Use -f to select file to send, default is a short message
+Use -f to select file to send, default is a short self.message
 
 On mininet, to send data to another host, please use -a <other host IP>, e.g. python ./client.py -a 10.0.0.2
 """
 
 from socket import *
 import argparse
-from os import urandom
-from struct import *
 import time
 import sys
 
-def main(rate, address, filename):
-    if filename:
-        with open(filename, 'rb') as input_file:
-            message = input_file.read()
-    else:
-        message = 'hi how are you'.encode('UTF-8')
+class Sender:
+    message = 'hi how are you'.encode('UTF-8') # default self.message
     
-    sock = socket(AF_INET, SOCK_DGRAM)
-    sock.settimeout(0.1)
-    receiver_address = (address, 5555)
+    def __init__(self, rate, address, filename, portnum):
+        self.updateFile(filename)
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.sock.settimeout(0.01)
+        self.receiver_address = (address, portnum)
+        self.mtu = 6400
+        self.packet_size = self.mtu
+        self.data_payload_size = self.packet_size - 28 - 13
+        self.timeinterval = self.packet_size/(rate*125000.0)
+        self.total_message_length = len(self.message)
+        self.tr_id = 353 # eventually unused
+        self.max_window_size = 5
+        self.window = [] # composite window. Only segIds that have not been ACKed will be in the window
+        self.nextSegId = 0 # the next segment id to add into the window
+        self.window_timer = []
+        self.w_timeout = 0.2
     
-    def segsend(segId, data_payload_size):
-        msg = message[segId:segId+data_payload_size] # spliced file contents
-        msg_length = len(msg)
-        f_newTransaction = 1 if segId == 0 else 0
-        f_endTransaction = 1 if segId+data_payload_size >= len(message) else 0
-        f_ack = 0
-        f_fin = 0 if window else 1
-        flags = f_newTransaction + (f_endTransaction << 1) + (f_ack << 2) + (f_fin << 3)
-        #print(flags)
-        #print('msg length %d' % msg_length)
-        payload = pack('!BIII' + str(msg_length) + 's', flags, tr_id, segId, len(message), msg) # struct pack is used to make sure that segId is always 4 bytes large, since integers in python are always 4 bytes
-        sent = sock.sendto(payload, receiver_address)
+    def reset(self):
+        self.mtu = 6400
+        self.packet_size = self.mtu
+        self.timeinterval = self.packet_size/(rate*125000.0)
+        self.total_message_length = len(self.message)
+        segId = 0
+        self.tr_id = 353 # eventually unused
+        self.max_window_size = 5
+        self.window = [] # composite window. Only segIds that have not been ACKed will be in the window
+        self.nextSegId = 0 # the next segment id to add into the window
+        self.window_timer = []
+        self.w_timeout = 0.2
+    
+    def sendPacket(self, segId, d_size):
+        # splice self.message to send
+        msg = self.message[segId:segId+d_size]
         
-        #print('segId:',segId,' sent:',sent-9)
+        # calculate flags
+        f_newTransaction = 1 if segId == 0 else 0
+        f_endTransaction = 1 if segId+d_size >= self.total_message_length else 0
+        f_ack = 0
+        f_fin = 0 if self.window else 1
+        flags = f_newTransaction + (f_endTransaction << 1) + (f_ack << 2) + (f_fin << 3)
+        
+        # encapsulate msg with our header
+        msg_length = len(msg)
+        payload = pack('!BIII' + str(msg_length) + 's', flags, self.tr_id, segId, len(self.message), msg) # struct pack is used to make sure that segId is always 4 bytes large, since integers in python are always 4 bytes
+        
+        # send packet
+        sent = self.sock.sendto(payload, self.receiver_address)
     
-    starttime = time.time()
-    
-    mtu = 6400
-    packet_size = mtu
-    data_payload_size = packet_size - 28 - 13
-    
-    timeinterval = packet_size/(rate*125000.0)
-    
-    nexttime = starttime - timeinterval # to make the client start sending data immediately instead of waiting for timeinterval
-    
-    segId = 0
-    tr_id = 353 # should be random
-    max_window_size = 5
-    
-    window = [] # composite window. Only segIds that have not been ACKed will be in the window
-    global nextSegId
-    nextSegId = 0 # the next segment id to add into the window
-    window_timer = []
-    w_timeout = 0.2
-    
-    # add stuff into the window
-    def addToWindow(nextSegId):
-        while len(window) < max_window_size and nextSegId < len(message):
-            if nextSegId + data_payload_size >= len(message):
-                data_size = len(message)-nextSegId
+    def addToWindow(self):
+        while len(self.window) < self.max_window_size and self.nextSegId < len(self.message):
+            if self.nextSegId + self.data_payload_size >= len(self.message):
+                data_size = len(self.message)-self.nextSegId
             else:
-                data_size = data_payload_size
-            window.append((nextSegId, data_size))
-            nextSegId += data_size
-            window_timer.append(time.time() - w_timeout)
-        return nextSegId
+                data_size = self.data_payload_size
+            self.window.append((self.nextSegId, data_size))
+            self.nextSegId += data_size
+            self.window_timer.append(time.time() - self.w_timeout)
     
-    nextSegId = addToWindow(nextSegId)
-    
-    def listenForAcks():
+    def listenForAcks(self):
         try:
-            data, addr = sock.recvfrom(1024)
+            data, addr = self.sock.recvfrom(1024)
             flags, recSegId, recMsgLen = unpack('!BII', data)
             f_fin = flags & 2**3
-            #print('fin',f_fin)
-            #print('window',window)
-            #print('segId',recSegId)
-            #print('msglen',recMsgLen)
-            if recSegId < len(message):
-                indexToChange = window.index((recSegId, recMsgLen))
-                window.pop(indexToChange)
-                window_timer.pop(indexToChange)
-                global nextSegId
-                nextSegId = addToWindow(nextSegId)
-            #print("\nreceived message:", data,"\nflags:", flags, "\nrecSegId:", recSegId, "\nrecMsgLen:", recMsgLen)
-            #print('w2',window)
+            if recSegId < len(self.message):
+                indexToChange = self.window.index((recSegId, recMsgLen))
+                self.window.pop(indexToChange)
+                self.window_timer.pop(indexToChange)
+                self.addToWindow()
             if f_fin:
                 return True
         except timeout:
             pass
         return False
-
-    while window:
-        if listenForAcks():
-            break
-        if (time.time() - nexttime) < timeinterval: continue
-        nexttime += timeinterval
-        
-        for i in range(len(window)):
-            if time.time() - window_timer[i] > w_timeout:
-                segId, data_payload_size = window[i]
-                segsend(segId, data_payload_size)
-                window_timer[i] = time.time()
-
-        #print('flags:',flags,'\npayload:',payload)
-        #print('sent %d bytes' % (sent))
-        sys.stdout.write("\rTime elapsed: %.3fs" % (time.time()-starttime))
-        sys.stdout.flush()
     
-    window_timer.append(time.time() - w_timeout)
-    while True:
-        if listenForAcks():
-            break
-        if (time.time() - nexttime) < timeinterval: continue
-        nexttime += timeinterval
-        
-        if time.time() - window_timer[0] > w_timeout:
-            segsend(len(message), 0)
-            window_timer[0] = time.time()
-
-        #print('flags:',flags,'\npayload:',payload)
-        #print('sent %d bytes' % (sent))
-        sys.stdout.write("\rTime elapsed: %.3fs" % (time.time()-starttime))
-        sys.stdout.flush()
+    def updateFile(self, filename):
+        if filename:
+            with open(filename, 'rb') as input_file:
+                self.message = input_file.read()
     
+    def sendFile(self):
+        starttime = time.time()
+        nexttime = starttime - self.timeinterval # to make the client start sending data immediately instead of waiting for self.timeinterval
+        self.addToWindow()
+        
+        while self.window:
+            self.listenForAcks()
+            if (time.time() - nexttime) < self.timeinterval: continue
+            nexttime += self.timeinterval
+            
+            for i in range(len(self.window)):
+                if time.time() - self.window_timer[i] > self.w_timeout:
+                    segId, d_size = self.window[i]
+                    self.sendPacket(segId, d_size)
+                    self.window_timer[i] = time.time()
+
+            sys.stdout.write("\rTime elapsed: %.3fs" % (time.time()-starttime))
+            sys.stdout.flush()
+        
+        self.window_timer.append(time.time() - self.w_timeout)
+        
+        while True:
+            if self.listenForAcks():
+                break
+            if (time.time() - nexttime) < self.timeinterval: continue
+            nexttime += self.timeinterval
+            
+            if time.time() - self.window_timer[0] > self.w_timeout:
+                self.sendPacket(len(self.message), 0)
+                self.window_timer[0] = time.time()
+
+            sys.stdout.write("\rTime elapsed: %.3fs" % (time.time()-starttime))
+            sys.stdout.flush()
+        print()
+    
+def main(rate, address, filename, portnum):
+    s = Sender(rate, address, filename, portnum)
+    s.sendFile()
 
 if __name__=="__main__":
-
     parser = argparse.ArgumentParser()
+    
     parser.add_argument('-r', type=float, dest='rate', default=3.0, 
-        help='Packet rate in Mbps (eg; -r 1.5 is 1.5 Mbps)')
+        help='Specify starting packet rate in Mbps (eg; -r 1.5 is 1.5 Mbps)')
     
     parser.add_argument('-a', type=str, dest='address', default='localhost', 
-        help='Indicate the ip address to send the data to')
+        help='Indicate the ip address to send the data to. Default is localhost. ')
     
     parser.add_argument('-f', type=str, dest='filename', default='', 
         help='Indicate the name of file to send')
     
+    parser.add_argument('-p', type=int, dest='portnum', default=5555, 
+        help='Indicate destination port. Default is 5555. ')
+    
     args = parser.parse_args()
     
-    print("Data rate is %.1f Mbps\nSending to %s" % (args.rate, args.address))
-    main(args.rate, args.address, args.filename)
+    print("Starting rate is %.1f Mbps\nSending to %s:%d" % (args.rate, args.address, args.portnum))
+    main(args.rate, args.address, args.filename, args.portnum)
 
 
 
